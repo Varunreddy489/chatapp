f263eb0b9ea97a1dbd491fd9afc8e1d14d029ba5
Varunreddy489 <varunsannapureddy@gmail.com>
2024-04-12 18:58:49 +0530

    new file:   frontend/.eslintrc.cjs 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/postcss.config.js 	new file:   frontend/public/bg.png 	new file:   frontend/public/vite.svg 	new file:   frontend/src/App.jsx 	new file:   frontend/src/components/messages/Message.jsx 	new file:   frontend/src/components/messages/MessageContainer.jsx 	new file:   frontend/src/components/messages/MessageInput.jsx 	new file:   frontend/src/components/messages/Messages.jsx 	new file:   frontend/src/components/sidebar/Conversation.jsx 	new file:   frontend/src/components/sidebar/Conversations.jsx 	new file:   frontend/src/components/sidebar/LogoutButton.jsx 	new file:   frontend/src/components/sidebar/SearchInput.jsx 	new file:   frontend/src/components/sidebar/Sidebar.jsx 	new file:   frontend/src/index.css 	new file:   frontend/src/main.jsx 	new file:   frontend/src/pages/home/Home.jsx 	new file:   frontend/src/pages/login/Login.jsx 	new file:   frontend/src/pages/signup/GenderCheckbox.jsx 	new file:   frontend/src/pages/signup/SignUp.jsx 	new file:   frontend/tailwind.config.js 	new file:   frontend/vite.config.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+// import Login from "./pages/login/Login";
+// import SignUp from "./pages/signup/SignUp";
+
+import Home from "./pages/home/Home";
+
+const App = () => {
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      {/* <Login /> */}
+      {/* <SignUp/> */}
+      <Home />
+    </div>
+  );
+};
+
+export default App;
